522f3d466cf00d95213f7df3fabd1590015a7dc6
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-03-20 18:37:17 +0100

    added event handling into testapp

--- integrations/testapp/cmd/testapp/main.go
+++ integrations/testapp/cmd/testapp/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -24,20 +25,69 @@ func main() {
 		},
 		AutharaBaseURL: "http://authara:8080",
 	})
-
 	if err != nil {
 		log.Fatalln("sdk not starting")
+	}
+
+	webhookSecret := os.Getenv("AUTHARA_WEBHOOK_SECRET")
+	if webhookSecret == "" {
+		log.Println("warning: AUTHARA_WEBHOOK_SECRET is empty, /webhooks/authara will reject requests")
 	}
 
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
 
 	r.Get("/", handlers.Home)
 
 	r.Group(func(r chi.Router) {
 		r.Use(appSdk.RequireAuthWithRefresh)
 		r.Get("/private", handlers.Private)
+	})
+
+	r.Post("/webhooks/authara", func(w http.ResponseWriter, r *http.Request) {
+		handler := &authara.WebhookHandler{
+			Secret: webhookSecret,
+		}
+
+		evt, err := handler.Handle(w, r)
+		if err != nil {
+			// Handle already wrote the HTTP error response.
+			log.Printf("webhook rejected: %v", err)
+			return
+		}
+
+		log.Printf(
+			"authara webhook received: id=%s type=%s created_at=%s data=%s",
+			evt.ID,
+			evt.Type,
+			evt.CreatedAt.Format("2006-01-02T15:04:05Z07:00"),
+			string(evt.Data),
+		)
+
+		switch evt.Type {
+		case authara.WebhookEventUserCreated:
+			data, err := authara.DecodeWebhookData[authara.UserCreatedData](evt)
+			if err != nil {
+				http.Error(w, "invalid user.created payload", http.StatusBadRequest)
+				return
+			}
+			log.Printf("user.created: user_id=%s", data.UserID)
+
+		case authara.WebhookEventUserDeleted:
+			data, err := authara.DecodeWebhookData[authara.UserDeletedData](evt)
+			if err != nil {
+				http.Error(w, "invalid user.deleted payload", http.StatusBadRequest)
+				return
+			}
+			log.Printf("user.deleted: user_id=%s", data.UserID)
+
+		default:
+			log.Printf("unknown webhook event type: %s", evt.Type)
+		}
+
+		w.WriteHeader(http.StatusNoContent)
 	})
 
 	log.Println("testapp listening on :8080")
