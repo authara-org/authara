8552e73ba8223e651734099e84b85c709210a57e
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-03-23 14:07:11 +0100

    correct type for roles

--- internal/http/kit/response/user.go
+++ internal/http/kit/response/user.go
@@ -4,18 +4,19 @@ import (
 	"time"
 
 	"github.com/authara-org/authara/internal/domain"
+	"github.com/authara-org/authara/internal/session/roles"
 )
 
 type User struct {
-	ID        string    `json:"id"`
-	Email     string    `json:"email"`
-	Username  string    `json:"username"`
-	Disabled  bool      `json:"disabled"`
-	CreatedAt time.Time `json:"created_at"`
-	Roles     []string  `json:"roles"`
+	ID        string       `json:"id"`
+	Email     string       `json:"email"`
+	Username  string       `json:"username"`
+	Disabled  bool         `json:"disabled"`
+	CreatedAt time.Time    `json:"created_at"`
+	Roles     []roles.Role `json:"roles"`
 }
 
-func UserWithRoles(u domain.User, roles []string) User {
+func UserWithRoles(u domain.User, roles []roles.Role) User {
 	return User{
 		ID:        u.ID.String(),
 		Email:     u.Email,
