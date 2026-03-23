11dba4deca4bae42afed935610e6deb49e5e1b82
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-03-23 14:50:19 +0100

    adjusted default values

--- internal/config/db.go
+++ internal/config/db.go
@@ -15,10 +15,10 @@ type DB struct {
 	Timezone string `env:"POSTGRESQL_TIMEZONE,default=UTC"`
 	LogSQL   bool   `env:"POSTGRESQL_LOG_SQL,default=false"`
 
-	MaxOpenConns    int           `env:"POSTGRESQL_MAX_OPEN_CONNS,default=25"`
-	MaxIdleConns    int           `env:"POSTGRESQL_MAX_IDLE_CONNS,default=10"`
-	ConnMaxLifetime time.Duration `env:"POSTGRESQL_CONN_MAX_LIFETIME,default=30m"`
-	ConnMaxIdleTime time.Duration `env:"POSTGRESQL_CONN_MAX_IDLE_TIME,default=5m"`
+	MaxOpenConns    int           `env:"AUTHARA_DB_MAX_OPEN_CONNS,default=40"`
+	MaxIdleConns    int           `env:"AUTHARA_DB_MAX_IDLE_CONNS,default=20"`
+	ConnMaxLifetime time.Duration `env:"AUTHARA_DB_CONN_MAX_LIFETIME,default=30m"`
+	ConnMaxIdleTime time.Duration `env:"AUTHARA_DB_CONN_MAX_IDLE_TIME,default=5m"`
 }
 
 func (db DB) validate() error {
