2bfe26d592f436c77a3089dca0a7d79a01a6b0cf
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-03-29 22:05:36 +0200

    actually implement funciton to check allowed emails

--- internal/store/allowed_emails.go
+++ internal/store/allowed_emails.go
@@ -1,10 +1,24 @@
 package store
 
-import "context"
+import (
+	"context"
+
+	"github.com/authara-org/authara/internal/store/model"
+)
 
 func (s *Store) IsEmailAllowed(ctx context.Context, email string) (bool, error) {
-	var allowed bool
-	return allowed, nil
+	var count int64
+
+	err := s.dbFromContext(ctx).
+		Model(&model.AllowedEmail{}).
+		Where("email = ?", email).
+		Count(&count).
+		Error
+
+	if err != nil {
+		return false, err
+	}
+	return count > 0, nil
 }
 
 func (s *Store) CreateAllowedEmail(ctx context.Context, email string) error {
