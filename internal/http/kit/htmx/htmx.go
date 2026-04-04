2369b161e37138af086bdd2c0dd84c35f9c718d7
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-04-04 15:45:33 +0200

    add helpers to write htmx headers

--- internal/http/kit/htmx/htmx.go
+++ internal/http/kit/htmx/htmx.go
@@ -5,6 +5,8 @@ import "net/http"
 const (
 	HTMXPushUrl    = "HX-Push-Url"
 	HTMXReplaceUrl = "HX-Replace-Url"
+	HTMXRetarget   = "HX-Retarget"
+	HTMXReswap     = "HX-Reswap"
 )
 
 func PushUrl(w http.ResponseWriter, url string) {
@@ -13,4 +15,12 @@ func PushUrl(w http.ResponseWriter, url string) {
 
 func ReplaceUrl(w http.ResponseWriter, url string) {
 	w.Header().Add(HTMXReplaceUrl, url)
+}
+
+func ReTarget(w http.ResponseWriter, target string) {
+	w.Header().Add(HTMXRetarget, target)
+}
+
+func ReSwap(w http.ResponseWriter, swap string) {
+	w.Header().Add(HTMXReswap, swap)
 }
