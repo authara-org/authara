9f2419126f5f5214b2541b0870e4d4d511c4c422
Alexander Lupatsiy <alexander@lupatsiy.de>
2026-04-08 20:07:41 +0200

    add email templates for password reset email

--- internal/email/template.go
+++ internal/email/template.go
@@ -17,3 +17,17 @@ func BuildSignupCodeMessage(code string) (Message, error) {
 	}
 	return msg, nil
 }
+
+func BuildPasswordResetCodeMessage(code string) (Message, error) {
+	htmlBody, err := RenderPasswordResetCodeHTML(code)
+	if err != nil {
+		return Message{}, err
+	}
+
+	msg := Message{
+		Subject: "Your password reset code",
+		Text:    templates.PasswordResetCodeText(code),
+		HTML:    htmlBody,
+	}
+	return msg, nil
+}
